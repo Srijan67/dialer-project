5380c8c894c98140cd0e4a057e0dd8618fd0ee87
Srijan67 <82119082+Srijan67@users.noreply.github.com>
2022-01-13 23:20:49 +0530

    proper commenting

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+//declaring and initialising variables.
 const dialer = document.querySelector(".dialer");
 const display = document.querySelector(".display");
 const nums = document.querySelectorAll(".num");
 const backspace = document.querySelector(".backspace");
 let test = "";
 
+//Loop to give functionality to all number keys using addEventListener.
 for (const num of nums) {
   num.addEventListener("click", () => {
     if (display.innerText == "O") {
@@ -12,6 +14,8 @@ for (const num of nums) {
     display.innerText = display.innerText + num.innerText;
   });
 }
+
+//Event to manage backspace key.
 backspace.addEventListener("click", () => {
   var displayArr = [].slice.call(display.innerText);
   const arrLength = displayArr.length;
